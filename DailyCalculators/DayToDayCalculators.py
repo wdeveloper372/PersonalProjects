7064d230c3a9330cb669db1a3b3e25676387251f
wdeleon1 <wdeleon2@students.avc.edu>
2025-05-12 13:31:44 -0700

    updated the profit

--- DailyCalculators/DayToDayCalculators.py
+++ DailyCalculators/DayToDayCalculators.py
@@ -1,6 +1,6 @@
 def run_calculation():
     parts = int(input("Enter the parts amount: "))
-    profit = parts * 40 / 100
+    profit = parts * 45 / 100
     sum_of_pars_and_profits = parts + profit
     tax = 11.25 / 100
     part_tax = sum_of_pars_and_profits * tax
@@ -15,7 +15,7 @@ def run_calculation_loop():
 
         response = input("Run another calculation? (yes/no): ")
 
-        if response not in ['yes', 'y']:
+        if response not in ["yes", "y"]:
             print("Exiting calculation program.")
             break  # Exit the loop
 
